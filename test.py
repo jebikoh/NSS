5f4c1acf0922b49c14441376c30051e560c67a44
Jayden E <aedara@uw.edu>
2024-03-28 23:39:32 -0700

    Add methods to read motion and depth

--- test.py
+++ test.py
@@ -4,12 +4,28 @@ from model import NeuralSuperSampling, ZeroUpsample
 from util import warp
 import matplotlib.pyplot as plt
 import numpy as np
+import os
+from util import read_motion
+
+os.environ["OPENCV_IO_ENABLE_OPENEXR"] = "1"
 
 if __name__ == "__main__":
-    color = torch.randn(2, 5, 3, 100, 100)
-    depth = torch.randn(2, 5, 1, 100, 100)
-    motion = torch.randn(2, 5, 2, 100, 100)
-    batch = (color, motion, depth)
+    file_path = "data/540p/motion/0009/0003.exr"
+    mh, mv = read_motion(file_path, negate_mv=True)
 
-    model = NeuralSuperSampling(2)
-    output = model(batch)
+    plt.figure(figsize=(10, 5))
+
+    plt.subplot(1, 2, 1)
+    plt.imshow(mv, cmap="viridis")
+    plt.colorbar(label="Vertical Velocity")
+    plt.title("Vertical Motion Vectors")
+    plt.axis("off")
+
+    plt.subplot(1, 2, 2)
+    plt.imshow(mh, cmap="viridis")
+    plt.colorbar(label="Horizontal Velocity")
+    plt.title("Horizontal Motion Vectors")
+    plt.axis("off")
+
+    plt.tight_layout()
+    plt.show()
--- util.py
+++ util.py
@@ -1,5 +1,9 @@
 import torch
 import torch.nn.functional as F
+import OpenEXR
+import Imath
+import numpy as np
+import PIL as Image
 
 
 def warp(input, flow):
@@ -27,3 +31,33 @@ def warp(input, flow):
     return F.grid_sample(
         input, vgrid.permute(0, 2, 3, 1), "bilinear", align_corners=True
     )
+
+
+def read_motion(file_path: str, negate_mv: bool = True):
+    exr_file = OpenEXR.InputFile(file_path)
+
+    header = exr_file.header()
+    dw = header["dataWindow"]
+    size = (dw.max.x - dw.min.x + 1, dw.max.y - dw.min.y + 1)
+
+    vertical_channel = exr_file.channel("R", Imath.PixelType(Imath.PixelType.FLOAT))
+    horizontal_channel = exr_file.channel("G", Imath.PixelType(Imath.PixelType.FLOAT))
+
+    mv = np.frombuffer(vertical_channel, dtype=np.float32).reshape(size[1], size[0])
+    mh = np.frombuffer(horizontal_channel, dtype=np.float32).reshape(size[1], size[0])
+
+    if negate_mv:
+        mv = -mv
+
+    return np.stack((mh, mv), axis=0)
+
+
+def read_depth(file_path: str):
+    depth_img = np.array(Image.open(file_path))
+    depth = (
+        depth_img[:, :, 0] / 255.0
+        + depth_img[:, :, 1] / 255.0**2
+        + depth_img[:, :, 2] / 255.0**3
+        + depth_img[:, :, 3] / 255.0**4
+    )
+    return depth
