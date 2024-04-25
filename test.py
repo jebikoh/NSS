741f050ed068cec4f9465dfe444544d77dd5e70f
Jayden E <aedara@uw.edu>
2024-04-24 20:41:53 -0700

    Fix in-place errors

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 import torch
 import matplotlib.pyplot as plt
 import os
+from model import NeuralSuperSampling
 from dataset import QRISPDataset
+from loss import WeightedSSIMPerceptualLoss
 
 os.environ["OPENCV_IO_ENABLE_OPENEXR"] = "1"
 
@@ -10,57 +12,20 @@ if __name__ == "__main__":
     dataset = QRISPDataset(data_path, split="train", sequence_length=5)
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True)
 
+    model = NeuralSuperSampling(2, 5, 10)
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+    loss = WeightedSSIMPerceptualLoss(0.1)
+
     for i, data in enumerate(dataloader):
-        (
-            color_frames,
-            motion_frames,
-            depth_frames,
-            trgt_frames,
-            sequence,
-            frame_idx,
-            f0,
-        ) = data
+        torch.autograd.set_detect_anomaly(True)
+        (color_frames, motion_frames, depth_frames, trgt_frames) = data
+        outputs = model(color_frames, motion_frames, depth_frames)
 
-        fig, (axs1, axs2) = plt.subplots(2, 5, figsize=(12, 6))
+        optimizer.zero_grad()
+        loss = loss(trgt_frames, outputs)
+        loss.backward()
+        optimizer.step()
 
-        fig.suptitle(
-            f"Sequence: {sequence[0]}, Frames: {frame_idx.item()} - {f0.item()}"
-        )
+        print("Loss:", loss.item())
 
-        # Display color frame 0
-        axs1[0].imshow(color_frames[0, 1].permute(1, 2, 0))
-        axs1[0].set_title("Color Frame 0")
-        axs1[0].axis("off")
-
-        # Display motion frame 0
-        axs1[1].imshow(motion_frames[0, 1, 0].unsqueeze(-1), cmap="viridis")
-        axs1[1].set_title("Horizontal Motion Frame -1")
-        axs1[1].axis("off")
-
-        axs1[2].imshow(motion_frames[0, 1, 1].unsqueeze(-1), cmap="viridis")
-        axs1[2].set_title("Vertical Motion Frame -1")
-        axs1[2].axis("off")
-
-        # Display depth frame 0
-        axs1[3].imshow(depth_frames[0, 0, 0], cmap="gray")
-        axs1[3].set_title("Depth Frame 0")
-        axs1[3].axis("off")
-
-        # Display target frame
-        axs1[4].imshow(trgt_frames[0].permute(1, 2, 0))
-        axs1[4].set_title("Target Frame")
-        axs1[4].axis("off")
-
-        axs2[0].imshow(color_frames[0, 0].permute(1, 2, 0))
-        axs2[0].set_title("Color Frame 0")
-        axs2[1].imshow(color_frames[0, 1].permute(1, 2, 0))
-        axs2[1].set_title("Color Frame -1")
-        axs2[2].imshow(color_frames[0, 2].permute(1, 2, 0))
-        axs2[2].set_title("Color Frame -2")
-        axs2[3].imshow(color_frames[0, 3].permute(1, 2, 0))
-        axs2[3].set_title("Color Frame -3")
-        axs2[4].imshow(color_frames[0, 4].permute(1, 2, 0))
-        axs2[4].set_title("Color Frame -4")
-
-        plt.tight_layout()
-        plt.show()
+        break
